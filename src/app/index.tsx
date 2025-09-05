8c2c40d11e86b15e55e9e9f4f6c2b345961c6b14
llnkps <99121995+llnkps@users.noreply.github.com>
2025-09-05 22:47:33 +0300

    Tailwind installation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import {SafeAreaProvider} from "react-native-safe-area-context";
 import AppNavigator from "@/src/navigation/AppNavigator";
 import {StatusBar} from "expo-status-bar";
+import '../styles/globals.css';
 
 export default function Index() {
   return (
